a94053af51c3ef15e8ef73bf7bbfed036b4fed34
Zeeshan Bhat <zeeshan.bhat@siemens-healthineers.com>
2018-10-02 17:16:40 -0400

    added Linked list data structure

--- src/modules/messaging.module.js
+++ src/modules/messaging.module.js
@@ -7,11 +7,85 @@
 let WS = require('ws');
 let MessageTypes = require('../defines/message_types');
 
+
+class Node {
+    constructor(prev, data, next){
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class NodeList {
+    constructor(){
+        this.head = null;
+
+    }
+    addFirst(data){
+        this.head = new Node(null, data, null);
+    }
+    
+    insertAfter(key, data){
+        let head = this.head;
+        let node;
+        while(head.data.id !== key && head.next !== null){
+            head = head.next;
+        }
+        if(head.next !== null){
+            node = new Node(head, data, head.next);
+            head.next.prev = node;
+            head.next = node;
+        }
+        
+    }
+    
+    insertBefore(key, data){
+        let head = this.head;
+        let node;
+        while(head.data.id !== key && head.next !== null){
+            head = head.next;
+        }
+            
+        node = new Node(head.prev, data, head);
+        head.prev.next = node;
+        head.prev = node;
+        
+        
+    }
+
+    deleteNode(key){
+        let head = this.head;
+        while(head.data.id !== key && head.next !== null){
+            head = head.next;
+        }
+        if(head.next !== null){
+            head.next.prev = head.prev;
+            head.prev.next = head.next;
+        }else{
+            head.prev.next = null;
+        }
+        // head has no reference and will be deleted by GC
+    }
+
+    appendNode(data){
+        if(this.head === null){
+            this.addFirst(data);
+        }else{
+            let head = this.head;
+            while(head.next !== null){
+                head = head.next;
+            }
+            head.next = new Node(head, data, null);
+        }
+
+    }
+}
+
 module.exports = class MessagingServer {
      constructor(ip, port){
         this.ip = ip;
         this.port = port;
-        this.clients = {}
+        this.clients = new NodeList()
         this.wsFlag = true;
      }
 
@@ -39,12 +113,24 @@ module.exports = class MessagingServer {
         this.server.on('connection', this.onConnectionHandler.bind(this));
      }
 
-     onMessageHandler(message){
+     onMessageHandler(message, client){
          try{
             let data = JSON.parse(message);
          
             switch(data.type){
-                // parse here
+                case MessageTypes.WS_DATA_TEXT_MESSAGE:
+                    if(!data.from){
+                        let res = {
+                            error: MessageTypes.ERR.WS_ERR_NO_ORIGIN,
+                            message: MessageTypes.ERR_DESC.WS_ERR_NO_ORIGIN
+                        }
+                        client.send(JSON.stringify(res));
+                    }
+                    this.sendMessage(message, message.to, client);
+                case MessageTypes.WS_META_CLIENT_ADD:
+                    this.addClient(message.id, client);
+                    break;
+                default:
             }
         } catch(e){
 
@@ -53,7 +139,7 @@ module.exports = class MessagingServer {
     }
 
      onConnectionHandler(ws){
-         ws.on('message', this.onMessageHandler.bind(this));
+         ws.on('message', message => { this.onMessageHandler(message, ws) });
      }
 
      setWSFlag(value){
@@ -76,6 +162,11 @@ module.exports = class MessagingServer {
      addClient(id, client){
         this.clients[id] = client;
      }
+
+     sendMessage(message, to, from){
+         message.from = from;
+         this.clients[to].send(message);
+     }
  }
 
 
@@ -93,7 +184,7 @@ module.exports = class MessagingClient{
          this.client.on('open', this.sendMessage.bind(this))
      }
 
-     sendMessage(){
+     sendMessage(message, to, from){
         this.client.send('Hello World!');
      }
 
